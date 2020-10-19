ad486ec531ae32cc0a7705a6bc028fce8fe99170
Fargui <gui.dumez@gmail.com>
2020-10-19 15:55:15 +0200

    mobile OK

---
+++ public/assets/js/app.js
@@ -0,0 +1,25 @@
+const menuButton = document.querySelector('.menu-button')
+const cross =  document.querySelector('.cross')
+const nav = document.querySelector('.nav')
+const filter = document.querySelector('.filter')
+let isVisible = false
+
+nav.style.display = "none"
+
+menuButton.addEventListener('click', ()=>{
+
+    isVisible = !isVisible;
+
+    if (isVisible) { // La cross doit etre presente
+        menuButton.setAttribute("src", "./images/icon-close.svg")
+        nav.style.display = "initial"
+        filter.style.display = "initial"
+
+    }else{ // le burger doit etre present
+        menuButton.setAttribute("src", "./images/icon-hamburger.svg")
+        nav.style.display = "none"
+        filter.style.display = "none"
+    }
+})
+
+
